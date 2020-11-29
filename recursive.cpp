254a3462343d361a205b5fe8298373a595a6d3f5
ConorDavenport <cnrdavenport@gmail.com>
2020-11-29 16:56:55 +0000

    made loads of changes, forgot to commit

--- recursive.cpp
+++ recursive.cpp
@@ -8,8 +8,11 @@
 #include <stdio.h>
 using namespace std;
 
+// track the number of zero elements at game start
+int zeroElements = 0;
+
 // get sudoku example game
-void parseData(string fileName, int* grid) {
+void parseData(string fileName, int* grid, int* lookup) {
   ifstream file;
   file.open(fileName);
   string field;
@@ -17,6 +20,15 @@ void parseData(string fileName, int* grid) {
   for (int i = 0; i < 81; i++) {
     getline(file, field, ','); // read char from example
     grid[i] = stoi(field);  // string to int
+
+    // if the current element is zero
+    // increase the size of lookup by one int
+    // record the index of the zero element 
+    if (grid[i] == 0) {
+      zeroElements++;
+      lookup = (int*)realloc(lookup, zeroElements * sizeof(int));
+      lookup[zeroElements - 1] = i;
+    }
   }
   file.close();
 }
@@ -29,11 +41,36 @@ void display(int* grid) {
   }
 }
 
+void check(int* grid) {
+  
+}
+
+void solve(int* grid, int n) {
+  if (n == 81) {
+    return;
+  }
+
+  if (grid[n] == 0)
+    cout << n << endl;
+  n++;
+  solve(grid, n);
+}
+
 int main(int argc, char* argv[]) {
   // init memory for grid
-  int* grid = (int*)malloc(81*sizeof(int));
+  int* grid = (int*)malloc(81 * sizeof(int));
+  // lookup table to keep track of which
+  // cells in the grid are zero at the start
+  // of the game
+  int* lookup = (int*)malloc(0);
 
-  parseData(argv[1], grid);
-  display(grid);
+  parseData(argv[1], grid, lookup);
+
+  for (int i = 0; i < zeroElements; i++) {
+    cout << lookup[i] << endl;
+  }
+
+  free(grid);
+  free(lookup);
   return 0;
 }
