5ac8e75bc488779ec1f55a8f8cbc04dbd0fb3263
ConorDavenport <cnrdavenport@gmail.com>
2020-12-31 01:06:21 +0000

    update comments

--- recursive.cpp
+++ recursive.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 // get sudoku example game
-// load game into grid
+// load game into grid, 1d vector
 void parseData(string fileName, vector<int>& grid) {
   ifstream file;
   file.open(fileName);
@@ -21,14 +21,17 @@ void parseData(string fileName, vector<int>& grid) {
   file.close();
 }
 
+// save vector v to file examples/solution.csv
 void save(vector<int> v) {
   ofstream file;
   file.open("examples/solution.csv");
   int i = 0;
+  // iterate through v, write elements to file
+  // insert new line after every ninth element
   for (vector<int>::const_iterator it = v.begin();
        it < v.end(); it++) { 
     i++;
-    file << *it << ","; 
+    file << *it << ",";
     if (0 == i%9) {
       file << endl;
     }
@@ -36,6 +39,7 @@ void save(vector<int> v) {
   file.close();
 }
 
+// return true if v contains no duplicates
 bool isUnique(vector<int> v) {
   sort(v.begin(), v.end());
   vector<int>::iterator it = unique(v.begin(), v.end());
@@ -46,6 +50,7 @@ bool isUnique(vector<int> v) {
   }
 }
 
+// print vector v to console
 void printVec(vector<int> v) {
   for (vector<int>::const_iterator i = v.begin(); 
        i != v.end(); ++i) {
@@ -54,16 +59,32 @@ void printVec(vector<int> v) {
   printf("\n");
 }
 
-// returns false if cell n doesn't fit
+// returns false if the value in cell n doesn't fit
+// the game solution
+// check() is called when a new value has been put
+// into cell n
+// params
+// grid   vector containing game board
+// n      index of element to be checked
 bool check(vector<int> grid, int n) {
+  // init vectors that will contain the non zero
+  // elements from the game board. These correspond
+  // to cell n's row, column, and 3x3 square
   vector<int> square, row, col;
   // check row
+  // grid is a 1d vector, find the index of
+  // the first element of cell n's row
   int rowStartIndex = n - n%9;
+  // iterate through row and fill vector row with all
+  // non zero elements
   for(int i = rowStartIndex; i < rowStartIndex + 9; i++) {
     if(grid[i] != 0)
       row.push_back(grid[i]);
   }
 
+  // check if the row contains duplicates
+  // the value that has been put into cell n will be
+  // valid if there are no duplicates
   if(!isUnique(row)) {
     return false;
   }
@@ -114,6 +135,7 @@ bool check(vector<int> grid, int n) {
   return true;
 }
 
+// find the next zero filled element in vector v
 int findZero(vector<int> v) {
   vector<int>::iterator it = find(v.begin(), v.end(), 0);
   if (it != v.end()) {
@@ -123,26 +145,41 @@ int findZero(vector<int> v) {
   }
 }
 
-// return codes
-// 0  no zero cells left
+// solve game
+// params
+// stack    lifo stack of game states
 int solve(vector<vector<int>>& stack) {
   // get working game state
   vector<int> grid = stack.back();
-  // find index of next zero cell to try numbers in
+  // find index of next zero filed cell to try numbers in
   int cell = findZero(grid);
+  // if there are no zero filled cells or if
+  // the function has gotten to the end of the grid
+  // return 0
   if (-1 == cell || cell > 80) {
     return 0;
   }
   // try numbers
   for (int i = 1; i < 11; i++) {
+    // input number i into the current zero filled cell
     grid[cell] = i;
+    // the end of the loop is reached when no number
+    // from 1 to 9 works in the current cell
     if (10 == i) {
+      // pop the current working state off the stack
+      // and return to the previous zero filled cell
       stack.pop_back();
       return 1;
     }
     // if number i works in the cell
     else if (check(grid, cell)) {
+      // push the found solution for current cell
+      // to the stack
       stack.push_back(grid);
+      // call solve() for the next zero filled cell
+      // if it returns zero, ie. it has reached the
+      // end of the game, break recursion by returning
+      // zero all the way up
       if (0 == solve(stack)) {
         return 0;
       }
@@ -155,13 +192,19 @@ int main(int argc, char* argv[]) {
   // init memory for grid
   vector<int> grid;
 
+  // load data from file into grid
   parseData(argv[1], grid);
 
-  // fifo stack to hold game states
+  // lifo stack to hold game states
   vector<vector<int>> stack;
+
+  // load starting game state into stack
   stack.push_back(grid);
+
+  // solve game
   solve(stack);
 
+  // perform final checks on solution
   for (int i = 0; i < grid.size(); i++) {
     if (!check(grid, i)) {
       printf("unsolved");
@@ -169,6 +212,7 @@ int main(int argc, char* argv[]) {
     }
   }
 
+  // save solution to examples/solution.csv
   save(stack.back());
   return 0;
 }
